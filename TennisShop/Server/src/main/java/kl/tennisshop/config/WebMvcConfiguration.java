c7a16125b923838a75089795ee6da28e590a7429
Kostadin Lambov <valchak@abv.bg>
2018-08-25 12:50:07 +0300

    Server bug fixed

--- TennisShop/Server/src/main/java/kl/tennisshop/config/WebMvcConfiguration.java
+++ TennisShop/Server/src/main/java/kl/tennisshop/config/WebMvcConfiguration.java
@@ -1,7 +1,6 @@
 package kl.tennisshop.config;
 
 import kl.tennisshop.web.interceptors.LogsInterceptor;
-import kl.tennisshop.web.interceptors.ResponseInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,18 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
     private final LogsInterceptor logsInterceptor;
-    private final ResponseInterceptor responseInterceptor;
 
 
-    public WebMvcConfiguration(LogsInterceptor logsInterceptor, ResponseInterceptor responseInterceptor) {
+    public WebMvcConfiguration(LogsInterceptor logsInterceptor) {
         this.logsInterceptor = logsInterceptor;
-        this.responseInterceptor = responseInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(this.logsInterceptor);
-        registry.addInterceptor(this.responseInterceptor);
 
     }
 
